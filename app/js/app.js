9b8112b1856f6ba5eb57c2ff16e706121e784b6b
leesangbin <sangbinlee9@gmail.com>
2016-04-10 05:49:19 +0900

    d

--- app/js/app.js
+++ app/js/app.js
@@ -2,27 +2,49 @@
 
 /* App Module */
 
-var phonecatApp = angular.module('phonecatApp', [
-  'ngRoute',
-  'phonecatAnimations',
+var phonecatApp = angular.module('phonecatApp', [ 'ngRoute',
+		'phonecatAnimations',
 
-  'phonecatControllers',
-  'phonecatFilters',
-  'phonecatServices'
-]);
+		'phonecatControllers', 'phonecatFilters', 'phonecatServices' ]);
 
-phonecatApp.config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-      when('/phones', {
-        templateUrl: 'partials/phone-list.html',
-        controller: 'PhoneListCtrl'
-      }).
-      when('/phones/:phoneId', {
-        templateUrl: 'partials/phone-detail.html',
-        controller: 'PhoneDetailCtrl'
-      }).
-      otherwise({
-        redirectTo: '/phones'
-      });
-  }]);
+phonecatApp.config([ 
+                     
+                     
+                     
+                     '$routeProvider'
+                     ,'$locationProvider'
+                     
+                     
+                     , function($routeProvider, $locationProvider) {
+	$routeProvider.when('/signin', {
+		templateUrl : 'partials/signin.html',
+		controller : 'signin'
+	}).when('/crud', {
+		templateUrl : 'partials/crud.html',
+		controller : 'crud'
+	}).when('/main', {
+		templateUrl : 'partials/main.html',
+		controller : 'main'
+	}).when('/', {
+		templateUrl : 'partials/main.html',
+		controller : 'main'
+	}).when('/phones', {
+		templateUrl : 'partials/phone-list.html',
+		controller : 'PhoneListCtrl'
+	}).when('/phones/:phoneId', {
+		templateUrl : 'partials/phone-detail.html',
+		controller : 'PhoneDetailCtrl'
+	}).otherwise({
+		redirectTo : '/main'
+	});
+	
+
+    // use the HTML5 History API
+    $locationProvider.html5Mode(true);
+	
+	
+}
+                     
+                     
+                     
+                     ]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,19 +4,37 @@
 
 var phonecatControllers = angular.module('phonecatControllers', []);
 
-phonecatControllers.controller('PhoneListCtrl', ['$scope', 'Phone',
-  function($scope, Phone) {
-    $scope.phones = Phone.query();
-    $scope.orderProp = 'age';
-  }]);
+phonecatControllers.controller('crud', [ '$scope', 'Phone',
+		function($scope, Phone) {
+			$scope.phones = Phone.query();
+			$scope.orderProp = 'age';
+		} ]);
+phonecatControllers.controller('main', [ '$scope', 'Phone',
+                                         function($scope, Phone) {
+	$scope.phones = Phone.query();
+	$scope.orderProp = 'age';
+} ]);
+phonecatControllers.controller('signin', [ '$scope', 'Phone',
+		function($scope, Phone) {
+			$scope.phones = Phone.query();
+			$scope.orderProp = 'age';
+		} ]);
 
-phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', 'Phone',
-  function($scope, $routeParams, Phone) {
-    $scope.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
-      $scope.mainImageUrl = phone.images[0];
-    });
+phonecatControllers.controller('PhoneListCtrl', [ '$scope', 'Phone',
+		function($scope, Phone) {
+			$scope.phones = Phone.query();
+			$scope.orderProp = 'age';
+		} ]);
 
-    $scope.setImage = function(imageUrl) {
-      $scope.mainImageUrl = imageUrl;
-    };
-  }]);
+phonecatControllers.controller('PhoneDetailCtrl', [ '$scope', '$routeParams',
+		'Phone', function($scope, $routeParams, Phone) {
+			$scope.phone = Phone.get({
+				phoneId : $routeParams.phoneId
+			}, function(phone) {
+				$scope.mainImageUrl = phone.images[0];
+			});
+
+			$scope.setImage = function(imageUrl) {
+				$scope.mainImageUrl = imageUrl;
+			};
+		} ]);
